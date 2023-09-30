05b96d27037c8b652a6e692f3e472230b2cbc88a
lasttrader <grishaperm@list.ru>
2023-09-30 14:00:41 +0300

    added API

---
+++ flask_app/api_message.py
@@ -0,0 +1,7 @@
+import requests
+
+message = requests.post('http://127.0.0.1:5000/flskapi/v1/add_message/', 
+                        json = {'X_get_age' : [10, 1, 0, 0, 1, 1, 0, 8, 3, 0, 0.12107185807539463, 0.3740520633526504, -0.1691938897666138, -0.5768294699140059, 2.9890440835684733, -0.3204128219555523]})
+print(message)
+if message.ok:
+    print(message.json()) #возвращение прогноза
--- flask_app/app.py
+++ flask_app/app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
-from  flask import Flask, request, render_template
+from  flask import Flask, request, render_template, jsonify
 
 #load models
 contact_le = pickle.load(open('models_tech/contact_le.pkl', 'rb'))
@@ -22,12 +22,14 @@ RFR = pickle.load(open('models_ml/RFR.pkl', 'rb'))
 #app
 app = Flask(__name__,  template_folder = 'templates')
 
+#роутинг
 @app.route('/', methods = ['POST', 'GET'])
 def main():
     if request.method == 'GET':
-        return render_template('main.html')
+        return render_template('main.html') #возвращаем стартовую страницу с формой
     
     if request.method == 'POST':
+        #Получаем жданные с формы html (поле name)
         contact = contact_le.transform([request.form['contact']])[0]
         default = default_le.transform([request.form['default']])[0]
         education = education_le.transform([request.form['education']])[0]
@@ -45,6 +47,7 @@ def main():
         pdays = float(request.form['pdays'])
         previous= float(request.form['previous'])
 
+        #собираем вектор категориальных признаков (порядок должен совпадать с порадокм, который был при обучении
         x_cat = [job,
                 marital,	
                 education,	
@@ -55,19 +58,31 @@ def main():
                 month,	
                 poutcome,	
                 y]
-
+        #Собираем вектор числовых признаков, порядок должен также совпадать
         x_num = num_scaler.transform([[balance, 
                 day, 
                 duration, 
                 campaign, 
                 pdays, 
                 previous]])[0]
+        #объединяем категориальные и числовые признаки для прогноза, порядок должен совпадать
         X = []
         X.extend(x_cat)
         X.extend(x_num)
+        print(X)
+        #прогноз
         prediction = RFR.predict([X])
         print(f'Примерный возраст клиента по введенным данных : {prediction}')
-        return render_template('main.html', result = prediction)
+        return render_template('main.html', result = prediction)#возвращаем прогноз в html  в поле result
+
+@app.route('/flskapi/v1/add_message/', methods = ['GET', 'POST']) #API
+def api_message():
+    x =  request.json #получаем json на входе из других сервисов
+    print(x['X_get_age'])
+    #нужнео сделать scaler и labelencoder, т.к. сторонний сервис не знает про масштабирование и кодирование даннеых дя модели
+    prediction = RFR.predict([x['X_get_age']])#прогноз
+
+    return jsonify(str(prediction)) #возвращаем результат в сервсис, который запросил прогноз
 
 
 if __name__ == '__main__':
